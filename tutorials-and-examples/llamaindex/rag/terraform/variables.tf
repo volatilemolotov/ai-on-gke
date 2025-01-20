d9b42dc247c4dad04671149a0db4689d3e72b705
Artur Kamalov <arttambel@gmail.com>
2025-01-20 07:12:46 +0100

    add manual and automated veersions of the tutorial

--- tutorials-and-examples/llamaindex/rag/terraform/variables.tf
+++ tutorials-and-examples/llamaindex/rag/terraform/variables.tf
@@ -1,8 +1,8 @@
 variable "project_id" {
   type = string
 }
-variable "create_cluster" {
-  type = bool
+variable "default_resource_name" {
+  type = string
 }
 variable "cluster_name" {
   type = string
@@ -10,13 +10,24 @@ variable "cluster_name" {
 variable "cluster_location" {
   type = string
 }
+variable "autopilot_cluster" {
+  type = bool
+}
 variable "private_cluster" {
   type = bool
 }
-
-
 variable "cluster_membership_id" {
   type        = string
   description = "require to use connectgateway for private clusters, default: cluster_name"
   default     = ""
 }
+variable "service_account_name" {
+  type=string
+}
+variable "bucket_name" {
+  type=string
+}
+variable "image_repository_name" {
+  type=string
+}
+
